ccf8e5d5e833562161ec83a23947d5b86faf2fde
Chris Chuter <cchuter@gmail.com>
2026-02-16 17:39:21 -0600

    feat: implement ParseOctal for octal-to-decimal conversion

--- go/exercises/practice/octal/octal.go
+++ go/exercises/practice/octal/octal.go
@@ -1,1 +1,14 @@
 package octal
+
+import "fmt"
+
+func ParseOctal(input string) (int64, error) {
+	num := int64(0)
+	for _, ch := range input {
+		if ch < '0' || ch > '7' {
+			return 0, fmt.Errorf("invalid octal digit: %c", ch)
+		}
+		num = num*8 + int64(ch-'0')
+	}
+	return num, nil
+}
