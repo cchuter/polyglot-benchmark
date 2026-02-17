65dd9019cbe30ec879105ccdb7abfb088c8d8527
Chris Chuter <cchuter@gmail.com>
2026-02-17 01:16:09 -0600

    bench: reset solution stubs

--- go/exercises/practice/octal/octal.go
+++ go/exercises/practice/octal/octal.go
@@ -1,16 +1,1 @@
 package octal
-
-import (
-	"fmt"
-)
-
-func ParseOctal(octal string) (int64, error) {
-	num := int64(0)
-	for _, digit := range octal {
-		if digit < '0' || digit > '7' {
-			return 0, fmt.Errorf("unexpected rune '%c'", digit)
-		}
-		num = num<<3 + int64(digit-'0')
-	}
-	return num, nil
-}
